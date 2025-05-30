999827061c1f0188c1fc9937c18c151cc17ce0e6
Funsho Ajayi <Funshoajayi29@gmail.com>
2025-05-30 01:34:53 +0100

    improved odds card

--- src/components/ui/OddsCard.tsx
+++ src/components/ui/OddsCard.tsx
@@ -4,18 +4,38 @@ interface Props {
 
 const OddsCard = ({ title }: Props) => {
   return (
-    <div className="bg-[#1B2D3B] flex flex-col gap-2 py-2 rounded-md">
+    <div className="bg-[#1B2D3B] hover:bg-[#11111180] duration-300 flex flex-col gap-4 py-3 px-2 rounded-md">
       <p className="text-[#D7DBE0] text-center font-medium">{title}</p>
-      <div className="flex justify-around gap-1">
-        <p className="border border-divide py-1 px-2 rounded">
+
+      <div className="flex justify-around  items-center">
+        <div className="flex flex-col items-center gap-1">
+          <div className="bg-[#D9D9D91A] rounded-full p-2">
+            <img src="./chelsea_logo.png" alt="chelsea logo" />
+          </div>
+          <p className="text-sm">Arsenal</p>
+        </div>
+        <div>
+          <p className="text-gray-400">Today</p>
+          <p className="text-sm">23:00</p>
+        </div>
+        <div className="flex flex-col items-center gap-1">
+          <div className="bg-[#D9D9D91A] rounded-full p-2">
+            <img src="./chelsea_logo.png" alt="chelsea logo" />
+          </div>
+          <p className="text-sm">Chelsea</p>
+        </div>
+      </div>
+
+      <div className="flex justify-around gap-1 text-sm">
+        <button className="border border-white/10 py-1 px-2 rounded flex gap-1">
           1 <span>1.22</span>
-        </p>
-        <p className="border border-divide py-1 px-2 rounded">
+        </button>
+        <button className="border border-white/10 py-1 px-2 rounded flex gap-1">
           X <span>1.3</span>
-        </p>
-        <p className="border border-divide py-1 px-2 rounded">
+        </button>
+        <button className="border border-white/10 py-1 px-2 rounded flex gap-1">
           2 <span>7.06</span>
-        </p>
+        </button>
       </div>
     </div>
   );
